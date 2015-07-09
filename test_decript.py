cf316e9caa20ea100af12c0be86bea84f4230e1d
George Moura <gwmoura@gmail.com>
2015-07-09 17:11:57 -0300

    add new test

--- test_decript.py
+++ test_decript.py
@@ -3,5 +3,13 @@ from caesar_cipher import *
 phrase = "George is smart"
 result = encrypt(phrase)
 
+print('ROT-0')
 print(result)
 print(decrypt(result))
+
+for n in range(0,25):
+	result = encrypt(phrase, n)
+	print('')
+	print('ROT-'+str(n))
+	print(result)
+	print(decrypt(result,n))
