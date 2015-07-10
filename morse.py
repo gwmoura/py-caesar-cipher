37b90dee31d8a78cfdbd0005b8b8fef911d28feb
Caio Moura <caiomoura1994@gmail.com>
2015-07-10 16:33:55 -0300

    add morse.py

---
+++ morse.py
@@ -0,0 +1,13 @@
+morse = {
+	'.-' : 'A',
+	'-'
+}
+
+'-.- . -.-- ... . -.-. ..- .-. .. - -.-- .- ..- ... . .- -. -.. .- ..- - .... . -. - .. -.-. .- - .. --- -. ... - . .--. - .-- --- .- -.-. - .. ...- .- - .'
+KEY SECURITY A USE AND AUTHENTICATION STEPT WO ACTIVATE
+
+def encrypt(phrase):
+  return new_phrase
+
+def descrypt(phrase):
+  return new_phrase
